06fe271e00a3c3b211a1f7f3cfefe90b34cc80b6
Maneesha Sane <ahseenam@gmail.com>
2024-11-12 13:55:08 -0500

    site vars for slack & topicbox guides

--- source/conf.py
+++ source/conf.py
@@ -197,7 +197,9 @@ myst_substitutions = {
   # General resources
   "glossary": "https://github.com/carpentries/community-engagement/blob/main/glossary.md",
   "code_of_conduct": handbook_url + "/policies/coc/",
-  "donate":"https://give.communityin.org/TheCarpentries",
+  "donate": "https://give.communityin.org/TheCarpentries",
+  "topicbox_guide": "https://docs.google.com/document/d/1lk1KmImG-5nkYzukjFMZMTS4BEVaOkqBg5_V_aj85_U/",
+  "slack_guide": "https://docs.google.com/document/d/1-NcL-ofnHXjJnopdWH6jLrdLHhDJeZO3/",
 
   # Websites
   "carpentries_website": "https://carp-new-website.netlify.app",
